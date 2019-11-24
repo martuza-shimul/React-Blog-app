49ab2e48ea4d0be148af2ab2bc042a271d4102d9
Martuza <martuza.shimul@gmail.com>
2019-11-25 00:26:32 +0600

    navbar display on sign in configured

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,23 +4,27 @@ import SignedinLinks from "./SignedinLinks";
 import SignedoutLinks from "./SignedoutLinks";
 import { connect } from "react-redux";
 
-const Navbar = () => {
-  return (
-    <nav className="nav-wrapper grey darken-4">
-      <div className="container">
-        <Link to="/" className="brand-logo">
-          MartPlan
-        </Link>
-        <SignedinLinks />
-        <SignedoutLinks />
-      </div>
-    </nav>
-  );
+const Navbar = props => {
+	const { auth } = props;
+	// console.log(auth);
+	const links = auth.uid ? <SignedinLinks /> : <SignedoutLinks />;
+	return (
+		<nav className="nav-wrapper grey darken-4">
+			<div className="container">
+				<Link to="/" className="brand-logo">
+					MartPlan
+				</Link>
+				{links}
+			</div>
+		</nav>
+	);
 };
 
 const mapStateToProps = state => {
-  console.log(state);
-  return {};
+	// console.log(state);
+	return {
+		auth: state.firebase.auth
+	};
 };
 
 export default connect(mapStateToProps)(Navbar);
