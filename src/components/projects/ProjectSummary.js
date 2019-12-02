09f99940cd3bb95478e41512b84072a72a725fda
Martuza <martuza.shimul@gmail.com>
2019-12-03 05:48:55 +0600

    author details added to the post

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-const ProjectSummary = ({project}) => {
-  return (
-    <div className="card z-depth-0 project-summary">
-      <div className="card-content grey-text text-darken-3">
-        <span className="card-title">{project.title}</span>
-        <p>Posted by Martuza</p>
-        <p className="grey-text">3rd November, 6pm</p>
-      </div>
-    </div>
-  );
+const ProjectSummary = ({ project }) => {
+	return (
+		<div className="card z-depth-0 project-summary">
+			<div className="card-content grey-text text-darken-3">
+				<span className="card-title">{project.title}</span>
+				<p>
+					Posted by {project.authorFirstName} {project.authorLastName}
+				</p>
+				<p className="grey-text">3rd November, 6pm</p>
+			</div>
+		</div>
+	);
 };
 
 export default ProjectSummary;
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,25 +1,27 @@
 // import { firestore } from '../../config/fbConfig'
 
 export const createProject = project => {
-  return (dispatch, getState, { getFirebase, getFirestore }) => {
-    // make async call to database
+	return (dispatch, getState, { getFirebase, getFirestore }) => {
+		// make async call to database
 
-    const firestore = getFirestore();
-    firestore
-      .collection("projects")
-      .add({
-        ...project,
-        authorFirstName: "Martuza",
-        authorLastName: "Shimul",
-        authorId: 3909,
-        createdAt: new Date()
-      })
-      .then(() => {
-        dispatch({ type: "CREATE_PROJECT", project });
-      })
-      .catch(err => {
-        dispatch({ type: "CREATE_PROJECT_ERROR", err });
-      });
-    // dispatch({ type: "CREATE_PROJECT", project });
-  };
+		const firestore = getFirestore();
+		const profile = getState().firebase.profile;
+		const authorId = getState().firebase.auth.uid;
+		firestore
+			.collection("projects")
+			.add({
+				...project,
+				authorFirstName: profile.firstName,
+				authorLastName: profile.lastName,
+				authorId: authorId,
+				createdAt: new Date()
+			})
+			.then(() => {
+				dispatch({ type: "CREATE_PROJECT", project });
+			})
+			.catch(err => {
+				dispatch({ type: "CREATE_PROJECT_ERROR", err });
+			});
+		// dispatch({ type: "CREATE_PROJECT", project });
+	};
 };
