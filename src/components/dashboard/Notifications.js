01c3fa9125c7b504b9c62573006b71a78732d50a
Martuza <martuza.shimul@gmail.com>
2019-12-03 07:07:43 +0600

    notification panel added

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-const Notifications = () => {
-  return (
-    <div className='white'>
-      <p>Notifications</p>
-    </div>
-  );
+const Notifications = props => {
+	return (
+		<div className="section">
+			<div className="card z-depth-0">
+				<div className="card-content">
+					<span className="card-title">Notifications</span>
+					<ul className="notifications">
+						<li>Notification</li>
+						<li>Notification</li>
+						<li>Notification</li>
+						<li>Notification</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Notifications;
