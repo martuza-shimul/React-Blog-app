7d5afb6d0989c55ad08ce6634a32951a98592673
Martuza <martuza.shimul@gmail.com>
2019-12-25 16:39:04 +0600

    product built for firebase hosting

---
+++ dist/precache-manifest.2c42b6f9d1257579e24958cd2ee4f065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14509cad846037721fdafb0f41720fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9624ebad76140899e3e3",
+    "url": "/static/css/main.682dd9c4.chunk.css"
+  },
+  {
+    "revision": "7910ac85a30cd3a2b9c7",
+    "url": "/static/js/2.5aff7a6a.chunk.js"
+  },
+  {
+    "revision": "9624ebad76140899e3e3",
+    "url": "/static/js/main.fdae0ecc.chunk.js"
+  },
+  {
+    "revision": "e90af7add8dbe7d09553",
+    "url": "/static/js/runtime-main.4da23071.js"
+  }
+]);
